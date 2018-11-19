58a7f0b6385b9f607753acd474539b7efc308420
maureenr1987 <maureenr1987@gmail.com>
2018-11-19 12:53:29 -0500

    Revert "Merge branch 'Maurico'"
    This reverts commit 3f63bc6bc824ab7128e8b78df5f335ad5a4e4345, reversing
    changes made to 9bb3587d175505ec5553e9eb2617b38a619b2613.

--- js/main.js
+++ js/main.js
@@ -113,12 +113,10 @@ var People = [ //Note User is always index 0
 
 
 //Items
-class ItemInfo{
-    constructor(Name, Price, Desc){
-        this.Name = Name
-        this.Price = Price
-        this.Desc = Desc
-    }
+function ItemInfo(Name, Price, Desc) {
+    this.Name = Name
+    this.Price = Price
+    this.Desc = Desc
 }
 var Items = [
     new ItemInfo("Hot Pocket", 2, "Need a delicious and satisfying snack? Hot Pockets® brand sandwiches are made with quality ingredients to deliver delicious taste and big flavor. +5 health"),
