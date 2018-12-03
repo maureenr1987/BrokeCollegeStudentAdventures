99c854b50b707cc78318fa2bb49f194af036f3c5
maureenr1987 <maureenr1987@gmail.com>
2018-12-03 12:16:54 -0500

    Quick Fix

--- js/main.js
+++ js/main.js
@@ -1046,7 +1046,7 @@ function DamageCalc(Perpetrator, Victim, Attack) {//Done
 
 function UseAttack(Perpetrator, Victim, Attack) {//Done
     //if the perpetrator is not the player then pick a random attack
-    if (Perpetrator != 0) {
+    if (Perpetrator != PlayerChar) {
         var Attack = Math.floor(Math.random() * 4)
     }
 
